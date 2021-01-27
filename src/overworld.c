05627287c0451882607af394ce7086bb52a9520e
Deokishisu <6993375+Deokishisu@users.noreply.github.com>
2021-01-27 13:49:54 -0500

    Silph's Music Changes After Rocket Defeat
    Silph Co.'s default music now changes to the GSC Saffron City theme after driving Team Rocket from the building.

--- src/overworld.c
+++ src/overworld.c
@@ -993,12 +993,20 @@ static u16 GetCurrLocationDefaultMusic(void)
 {
     u16 music;
     music = GetLocationMusic(&gSaveBlock1Ptr->location);
+    if(music == MUS_SILPH && FlagGet(FLAG_HIDE_SILPH_ROCKETS))
+    {
+        music = MUS_GSC_PEWTER;
+    }
     return music;
 }
 
 static u16 GetWarpDestinationMusic(void)
 {
     u16 music = GetLocationMusic(&sWarpDestination);
+    if(music == MUS_SILPH && FlagGet(FLAG_HIDE_SILPH_ROCKETS))
+    {
+        music = MUS_GSC_PEWTER;
+    }
     return music;
 }
 
