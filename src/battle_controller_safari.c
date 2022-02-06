97c86f5c3c7fc8e9d70229a7e9cc32e1ef819ef7
Deokishisu <6993375+Deokishisu@users.noreply.github.com>
2022-02-06 00:40:50 -0500

    B Now Moves Selection to Run in Safari Battles
    This was overlooked when implementing B button moving the cursor to run in wild battles, as Safari Battles use a different battle controller file.

--- src/battle_controller_safari.c
+++ src/battle_controller_safari.c
@@ -222,6 +222,13 @@ static void HandleInputChooseAction(void)
             ActionSelectionCreateCursorAt(gActionSelectionCursor[gActiveBattler], 0);
         }
     }
+    else if (JOY_NEW(B_BUTTON))
+    {
+        PlaySE(SE_SELECT);
+        ActionSelectionDestroyCursorAt(gActionSelectionCursor[gActiveBattler]);
+        gActionSelectionCursor[gActiveBattler] = 3;
+        ActionSelectionCreateCursorAt(gActionSelectionCursor[gActiveBattler], 0);
+    }
 }
 
 static void CompleteOnBattlerSpriteCallbackDummy(void)
